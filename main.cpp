8df6975474702ff7559e175cbdaa9bf3bb19d9a2
cao1629 <hengcao0556@gmail.com>
2025-07-27 17:26:34 -0400

    Sun Jul 27 17:26:34 EDT 2025

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#include <vector>
 #include <iostream>
+#include <unordered_map>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
+int main(int argc, char const *argv[]) {
+  std::unordered_map<int, int> m{
+    {1, 2},
+    {2, 3},
+    {3, 4}
+  };
+
+  std::cout << m.size() << std::endl;
   return 0;
 }
